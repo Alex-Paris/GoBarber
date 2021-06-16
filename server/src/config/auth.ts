0e654504dd9b7885a32d59a6f9c699922fdeeec0
Alex Paris <alexlima@gruposgapetro.com.br>
2021-06-16 01:58:04 -0300

    initial commit

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a5a9d28e8cf460a7cb09983e94c549c2',
+    expiresIn: '1d'
+  }
+};
---
+++ server/src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+import auth from '../config/auth';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+};
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<{user: User, token: string}> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email }});
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    };
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    };
+
+    const { secret, expiresIn } = auth.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn
+    });
+
+    return {user, token};
+  };
+};
+
+export default AuthenticateUserService;

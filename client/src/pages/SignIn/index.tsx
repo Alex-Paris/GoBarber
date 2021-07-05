b75f837612ff6e55098b434eccecc976789e5110
Alex Paris <alexlima@gruposgapetro.com.br>
2021-07-05 06:39:13 -0300

    feat: react router dom and redirect history

--- client/src/pages/SignIn/index.tsx
+++ client/src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/context/auth';
 import { useToast } from '../../hooks/context/toast';
@@ -13,7 +14,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -25,6 +26,7 @@ const SignIn: React.FC = () => {
 
   const { signIn } = useAuth();
   const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -38,50 +40,53 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false
       });
+
+      await signIn({
+        email: data.email,
+        password: data.password
+      });
+
+      history.push('/dashboard');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
 
         formRef.current?.setErrors(errors);
-      }
-      return;
-    }
 
-    try {
-      await signIn({
-        email: data.email,
-        password: data.password
-      });
-    } catch (err) {
+        return;
+      }
+
       addToast({
         type: 'error',
         title: 'Erro na autenticação',
         description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
       });
     }
-  }, [signIn, addToast]);
+  }, [signIn, addToast, history]);
 
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input name="password" icon={FiLock} placeholder="Senha" />
+            <Input name="password" icon={FiLock} placeholder="Senha" />
 
-          <Button type="submit">Entrar</Button>
+            <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
 
-        <a href="">
-          <FiLogIn />
+          <Link to="/signup">
+            <FiLogIn />
           Criar conta
-        </a>
+          </Link>
+        </AnimationContainer>
       </Content>
 
       <Background />

6067336dfa1a77282257ab441cc0a835468bcd42
Alex Paris <alexlima@gruposgapetro.com.br>
2021-09-06 06:23:46 -0300

    feat: aws ses

---
+++ server/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'alex.paris@pariscode.dev',
+      name: 'Alex - Paris Code'
+    }
+  }
+} as IMailConfig;

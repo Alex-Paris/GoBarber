b697dfd921d273a63e4985438fee195890becf7d
Alex Paris <alexlima@gruposgapetro.com.br>
2021-09-07 09:17:34 -0300

    chore: cache for list provider appointment

--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -22,18 +22,22 @@ class ListProviderAppointmentsService {
   ) {}
 
   public async execute({ provider_id, year, month, day }: IRequestDTO): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey);
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-      provider_id,
-      year,
-      month,
-      day
-    });
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+        provider_id,
+        year,
+        month,
+        day
+      });
 
-    // await this.cacheProvider.save('asd', 'qwe');
+      console.log('feito truta');
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   };

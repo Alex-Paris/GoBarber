6d8638b1d925e7311e5d1dc58632fd2fad0de881
Alex Paris <alexlima@gruposgapetro.com.br>
2021-09-08 06:41:00 -0300

    ghost: buuu!

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -41,5 +41,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-	console.log('👻 Server started on port 3333!');
+	console.log('👻 Buuu! Server started on port 3333!');
 });

3692a12518db0973a771536a9510addaef48663b
Alex Paris <alexlima@gruposgapetro.com.br>
2021-09-09 07:01:46 -0300

    chore: schedule template

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import { useAuth } from '../../hooks/context/auth';
 import logoImg from '../../assets/logo.svg';
@@ -31,6 +31,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 19</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://yt3.ggpht.com/yti/APfAmoENyWa8B8szMSqFCAwKMUgu4pCgcFqUADbW_rmGCA=s88-c-k-c0x00ffffff-no-rj-mo"
+                alt="Alex"
+              />
+
+              <strong>Alex</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
